d906e8b16bc941cc1b56ccc1fa7ed1c2516a4114
Anderson <andersonst.dev@gmail.com>
2022-03-03 22:15:55 -0300

    test: remove post file

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from 'jest-mock'
-import { stripe } from '../../services/stripe'
 
 import Post, { getServerSideProps } from "../../pages/posts/[slug]";
 import { getPrismicClient } from '../../services/prismic'
